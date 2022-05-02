8e4340820a686c3a85baa4e627253144fa53f079
darlanpontin <darlan.pontin95@gmail.com>
2022-05-02 14:21:01 -0300

    Atualização do nome do objeto Guerreiro

--- src/RPG.java
+++ src/RPG.java
@@ -3,15 +3,15 @@ import java.util.Random;
 public class RPG {
 	public static void main(String[] args) {
 		Mago gandalf = new Mago("Gandalf", 60, 18, 8);
-		Guerreiro mike = new Guerreiro("Thor", 80, 8, 18);
+		Guerreiro thor = new Guerreiro("Thor", 80, 8, 18);
 		
 		gandalf.apresentar();
-		mike.apresentar();
+		thor.apresentar();
 		System.out.println("");
 		
-		mike.aprenderHabilidade("Soco");
-		mike.aprenderHabilidade("Chute");
-		mike.aprenderHabilidade("Cotovelada");
+		thor.aprenderHabilidade("Soco");
+		thor.aprenderHabilidade("Chute");
+		thor.aprenderHabilidade("Cotovelada");
 		
 		gandalf.aprenderMagia("Bola de fogo");
 		gandalf.aprenderMagia("Chuva ácida");
@@ -26,39 +26,39 @@ public class RPG {
 			if (r == 0) {
 				System.out.println("O Mago ataca primeiro!");
 				gandalf.attack();
-				mike.setVida(mike.getVida() - gandalf.getMagia());
-				if(mike.getVida() <= 0) {
+				thor.setVida(thor.getVida() - gandalf.getMagia());
+				if(thor.getVida() <= 0) {
 					break;
 				}
-				mike.attack();
-				gandalf.setVida(gandalf.getVida() - mike.getHabilidade());
+				thor.attack();
+				gandalf.setVida(gandalf.getVida() - thor.getHabilidade());
 			} else {
 				System.out.println("O Guerreiro ataca primeiro!");
-				mike.attack();
-				gandalf.setVida(gandalf.getVida() - mike.getHabilidade());
+				thor.attack();
+				gandalf.setVida(gandalf.getVida() - thor.getHabilidade());
 				if(gandalf.getVida() <= 0) {
 					break;
 				}
 				gandalf.attack();
-				mike.setVida(mike.getVida() - gandalf.getMagia());
+				thor.setVida(thor.getVida() - gandalf.getMagia());
 			}
 			System.out.println("Vida do " + gandalf.getNome() + " : " +
 								gandalf.getVida());
-			System.out.println("Vida do " + mike.getNome() + " : " + 
-								mike.getVida());
+			System.out.println("Vida do " + thor.getNome() + " : " + 
+								thor.getVida());
 			System.out.println("");
-		} while (gandalf.getVida() > 0 && mike.getVida() > 0);
+		} while (gandalf.getVida() > 0 && thor.getVida() > 0);
 				System.out.println("Vida do " + gandalf.getNome() + " : " +
 									gandalf.getVida());
-				System.out.println("Vida do " + mike.getNome() + " : " + 
-									mike.getVida());	
+				System.out.println("Vida do " + thor.getNome() + " : " + 
+									thor.getVida());	
 		
 		System.out.println("====================================");
 		System.out.println("Fim da luta!!");
 		
 		if(gandalf.getVida() > 0) {
 			System.out.println("O MAGO É O MAGO É O VENCEDOR!!");
-		}else if (mike.getVida() > 0) {
+		}else if (thor.getVida() > 0) {
 			System.out.println("O GUERREIRO É O VENCEDOR!!");
 		}
 		
